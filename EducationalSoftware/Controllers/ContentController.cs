03039ef7edcdfd138e972115683290bba5f88e13
Anthony_Kalampogias <antonykalampogias@gmail.com>
2022-06-25 23:48:17 +0300

    refreshed ignored files.

--- EducationalSoftware/Controllers/ContentController.cs
+++ EducationalSoftware/Controllers/ContentController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EducationalSoftware.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,12 +11,26 @@ namespace EducationalSoftware.Controllers
     {
         public ActionResult Index(int? contentID = null)
         {
-
-            if(contentID != null)
+            if(contentID == null)
             {
-
+                ViewData["error"] = "You need to select a chapter to read first!";
+                return RedirectToAction("Index", "Home");
             }
 
+            var content = new Content();
+            using (var db = new SoftwareEduEntities())
+                content = db.Content.FirstOrDefault(x => x.Id == contentID);
+            return View(content);
+        }
+
+
+        public ActionResult Quiz(int? contentID = null)
+        {
+            if (contentID == null)
+            {
+                ViewData["error"] = "You need to select a chapter to start a quiz!";
+                return RedirectToAction("Index", "Home");
+            }
             return View();
         }
     }
